a42f333ce91dc5f8921b963179db1bdf2e71df4d
izzyjosh <calebayuba222@gmail.com>
2023-11-12 19:11:48 +0100

    wrote a models for friend request

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import uuid
 
-# Create your models here.
+class Frequest(models.Model):
+    id = models.UUIDField(
+            primary_key=True,
+            unique=True,
+            default=uuid.uuid4,
+            editable=False,)
+    sender = models.ForeignKey(
+            User,
+            on_delete=models.CASCADE,
+            related_name="sender")
+    receiver = models.ForeignKey(
+            User,
+            on_delete=models.CASCADE,
+            related_name="receiver")
+    status = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"form {self.sender.username} to {self.receiver.username}"
