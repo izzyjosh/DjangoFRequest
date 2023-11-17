b6f13857a9ff85da6b8c32a4f725d45efc769ccb
izzyjosh <calebayuba222@gmail.com>
2023-11-17 11:48:37 +0100

    styled the reset password forms using bootsrap

--- FRequest/urls.py
+++ FRequest/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from account.forms import CustomPasswordResetForm,CustomSetPasswordForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,20 +9,26 @@ urlpatterns = [
     path("account/",include("account.urls")),
     path(
         "password_reset_view/",
-        auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+        auth_views.PasswordResetView.as_view(
+            template_name="password_reset.html",
+            form_class=CustomPasswordResetForm),
         name="password_reset"),
 
     path(
         "password_reset_done/",
-        auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_done.html"),
         name="password_reset_done"),
     path(
         "password_reset_confirm/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password_reset_confirm.html",
+            form_class=CustomSetPasswordForm),
         name="password_reset_confirm"),
     path(
         "password_reset_complete/",
-        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password_reset_complete.html"),
         name="password_reset_complete")
 
 ]
--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,60 @@
 from django import forms
 from .models import MyUser
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm,SetPasswordForm
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+    email = forms.EmailField(
+            label="",
+            required=True,
+            widget = forms.EmailInput(attrs={
+                "placeholder":"Valid Email",
+                "type":"email",
+                "name":"email",
+                "class":"form-control m-2",
+                })
+            )
+    class Meta:
+        model = MyUser
+        fields = ("email")
+
+class CustomSetPasswordForm(SetPasswordForm):
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+    newpassword1 = forms.CharField(
+        label="",
+        required=True,
+        help_text="",
+        widget = forms.PasswordInput(attrs={
+            "placeholder":"password",
+            "type":"password",
+            "name":"password1",
+            "class":"form-control m-2",
+            })
+        )
+
+    newpassword2 = forms.CharField(
+        label="",
+        required=True,
+        help_text="",
+        widget = forms.PasswordInput(attrs={
+            "placeholder":"confirm password",
+            "type":"password",
+            "name":"password1",
+            "class":"form-control m-2",
+            })
+        )
+
+    class Meta:
+        model = MyUser
+        fields = ("newpassword1","newpassword2")
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -59,3 +113,7 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = MyUser
         fields = ("email","username")
+
+
+
+
